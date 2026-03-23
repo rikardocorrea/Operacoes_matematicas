d01bf1be7cad7e3fc974b94fd2b4c9be7e0c59e0
Instrutor <prof.ricardo.sbo@gmail.com>
2026-03-23 13:57:36 -0300

    Com a adicao funcionando

--- scripts.js
+++ scripts.js
@@ -1,2 +1,4 @@
 let numero1 = Number(prompt("Digite o primeiro número: "))
 let numero2 = Number(prompt("Digite o segundo número: "))
+let adicao = numero1 + numero2
+alert(`O resultado da soma é: ${adicao}`)
